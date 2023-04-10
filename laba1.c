15d5ca5382f1d0c6a0b0f627ac64e7f5078c1061
Anastasia Bondarenko <jumkot@mail.ru>
2023-04-10 22:55:23 +0700

    Format laba1.c

--- laba1.c
+++ laba1.c
@@ -50,7 +50,10 @@ void show_error(int ERROR, int num, char* ch)
             break;
         }
     case ERROR_NUMBER:
-        printf("Error at column %d: expected %s'<double>'%s\n", num, BLUE, CLOSE);
+        printf("Error at column %d: expected %s'<double>'%s\n",
+               num,
+               BLUE,
+               CLOSE);
         break;
     case ERROR_UNEXPECT_TOKEN:
         printf("Error at column %d: unexpected token\n", num);
@@ -73,15 +76,18 @@ void skip_space(char* arr, int* num, char ch)
     while (arr[*num] != ch) {
         if (arr[*num] == ' ') {
             *num += 1;
-        } else if ((ch == '0') && (isdigit(arr[*num]) || arr[*num] == '-' || arr[*num] == '.')){
+        } else if (
+                (ch == '0')
+                && (isdigit(arr[*num]) || arr[*num] == '-'
+                    || arr[*num] == '.')) {
             break;
-        } else if (ch == ','){
+        } else if (ch == ',') {
             show_error(ERROR_EXPECT_COMMA, *num, NULL);
             exit(EXIT_FAILURE);
-        } else if (ch == ')'){
+        } else if (ch == ')') {
             show_error(ERROR_BRACKET, *num, &arr[*num]);
             exit(EXIT_FAILURE);
-        } else if (ch == '\n'){
+        } else if (ch == '\n') {
             show_error(ERROR_UNEXPECT_TOKEN, *num, NULL);
             exit(EXIT_FAILURE);
         }
@@ -92,7 +98,6 @@ double number_search(char* arr, int* num)
 {
     char* exp = malloc(20 * sizeof(char));
     int i = 0;
-    
     while (isdigit(arr[*num]) || arr[*num] == '-' || arr[*num] == '.') {
         if (arr[*num] == '.' && arr[*(num + 1)] == '.') {
             show_error(ERROR_NUMBER, *num, NULL);
@@ -102,7 +107,6 @@ double number_search(char* arr, int* num)
         i++;
         *num += 1;
     }
-    
     char* trash;
     double Number = strtod(exp, &trash);
     free(exp);
@@ -133,21 +137,19 @@ struct circle data_of_figure(char* arr, int* num)
 
 void show_figure(struct circle* Circle, char* figure)
 {
-    printf("\n%s(%.2f %.2f, %.2f)\n", 
-        figure,
-        Circle->center.x,
-        Circle->center.y,
-        Circle->radius);
+    printf("\n%s(%.2f %.2f, %.2f)\n",
+           figure,
+           Circle->center.x,
+           Circle->center.y,
+           Circle->radius);
 }
 
 int main()
 {
     char enter[SIZE], figure[SIZE];
     int num = 0;
-
     printf("Enter a geometric figure (or press Enter for exit):\n");
     fgets(enter, SIZE, stdin);
-    
     for (int i = 0; i < strlen(enter); i++) {
         if (enter[i] == '(') {
             to_lower(figure, num);
@@ -159,11 +161,10 @@ int main()
                 show_error(ERROR_NAME, 0, NULL);
                 exit(EXIT_FAILURE);
             }
-        } else if (enter[i] == ')' || enter[i] == ' '){
+        } else if (enter[i] == ')' || enter[i] == ' ') {
             show_error(ERROR_BRACKET, num, &enter[i]);
             exit(EXIT_FAILURE);
         }
-
         figure[num] = enter[i];
         num++;
     }
